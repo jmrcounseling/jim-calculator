a60770dada8653b8929819f591d040ee0fbeb912
James Reinach <jmrcounseling@gmail.com>
2015-01-08 21:05:43 +0000

    Calculator

---
+++ jim_calculator1.rb
@@ -0,0 +1,67 @@
+loop do
+  
+def say(msg)
+  puts " => #{msg}"
+end
+
+say "What's the first number?"
+num1 = gets.chomp
+say "What's the second number?"
+num2 = gets.chomp
+
+say  "Enter the corresponding number to the type of calulation you want to perform: 1) add  2) subtract  3) multiply  4) divide"
+operator = gets.chomp
+
+if operator == "1"
+  result = num1.to_i + num2.to_i
+elsif operator == "2"
+  result = num1.to_i - num2.to_i
+elsif operator == "3"
+  result = num1.to_i * num2.to_i
+else operator == "4"
+  result = num1.to_i / num2.to_f
+end
+
+result = "#{result}"
+say "Your result is #{result}"
+
+  say "Do you have another calculation?"
+  answer = gets.chomp
+   if answer == "Yes" || answer == "yes"
+   else
+     puts "Good-bye for now."
+     break
+   end
+end
+     
+  
+  
+
+
+  
+
+  
+  
+    
+
+  
+  
+  
+    
+  
+  
+ 
+
+ 
+      
+      
+          
+ 
+  
+  
+    
+  
+  
+  
+  
+  
